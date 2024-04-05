b1ed14ae1bd5c67fe600fd85d015f16aabac94e6
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-04-05 11:26:33 +0530

    Create Test.js component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import Navbar from '../components/Navbar';
+import Navbar from "../components/Navbar";
 import Hero from "./Hero";
 import GridCard from "./GridCard";
 import Testimonials from "../components/Testimonials";
 import Footer from "../components/Footer";
 
 const Home = () => {
- return (
-    <div style={{ backgroundColor: '#f2f2f2', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      
-      
-      <Hero/>
-      <div style={{ flex: 1, overflow: 'auto' }}>
-        <GridCard/>
+  return (
+    <div
+      style={{
+        backgroundColor: "#f2f2f2",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Hero />
+      <div style={{ flex: 1, overflow: "auto" }}>
+        <GridCard />
       </div>
-      <Testimonials/>
-      <Footer/>
+      <Testimonials />
+      <Footer />
     </div>
- );
+  );
 };
 
 export default Home;
